714a54baf0ad5eadab975b2470bc89c694efcc1d
知梦寒 <inkedawn@gmail.com>
2020-03-17 11:04:43 +0800

    补全RoleColor

--- connection/Role.go
+++ connection/Role.go
@@ -7,7 +7,14 @@ import (
 type RoleColor = uint32
 
 var (
-	RoleGreen RoleColor = 0x0000FF00
+	RoleYellow RoleColor = 0xFFFF00
+	RoleGreen  RoleColor = 0x00FF00
+	RoleRed    RoleColor = 0xFD3E3E
+	RoleOrange RoleColor = 0xFF6500
+	RoleBlue   RoleColor = 0x0000FF
+	RoleBrown  RoleColor = 0x996600
+	RoleWhite  RoleColor = 0xFFFFFF
+	RoleBlack  RoleColor = 0x000000
 )
 
 func (c *Connection) CreateRole(nickname [16]byte, color RoleColor) *promise.Promise {
