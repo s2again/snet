5e8aba1e4235eb96dba8b100056ac7927b2a5855
知梦寒 <inkedawn@gmail.com>
2020-04-22 11:05:04 +0800

    修复登录

--- Login.go
+++ Login.go
@@ -10,11 +10,12 @@ import (
 	"github.com/s2again/snet/core"
 )
 
-type LoginResponseFromGuide struct {
+type LoginResponse struct {
 	SessionID   [16]byte
 	RoleCreated bool
 }
 
+// unimplemented
 func (c *GuideServerConnection) LoginByEmail(email string, password string) (prom *promise.Promise) {
 	const channel uint32 = 0
 	const gameType uint32 = 1
@@ -26,6 +27,8 @@ func (c *GuideServerConnection) LoginByEmail(email string, password string) (pro
 			resp, err := parseLoginResponseFromGuide(body)
 			if err == nil {
 				log.Printf("LoginResponse %X\n", v.(core.PacketBody).Bytes())
+				// todo: 从head获取uid
+				// c.SetSession(uid, resp.SessionID)
 				prom.Resolve(resp)
 			} else {
 				prom.Reject(fmt.Errorf("LoginByEmail Rejected, reason: %v", err))
@@ -37,16 +40,18 @@ func (c *GuideServerConnection) LoginByEmail(email string, password string) (pro
 	return prom
 }
 
-func (c *GuideServerConnection) Login(password string) (prom *promise.Promise) {
+func (c *GuideServerConnection) Login(uid uint32, password string) (prom *promise.Promise) {
 	const channel uint32 = 0
 	const gameType uint32 = 1
 	prom = promise.NewPromise()
+	c.SetSession(uid, [16]byte{})
 	c.SendInPromise(Command_MAIN_LOGIN_IN, pwdHashBytes(password), channel, gameType, uint32(0)).
 		OnSuccess(func(v interface{}) {
 			body := v.(core.PacketBody)
 			resp, err := parseLoginResponseFromGuide(body)
 			if err == nil {
 				log.Printf("LoginResponse %X\n", v.(core.PacketBody).Bytes())
+				c.SetSession(uid, resp.SessionID)
 				prom.Resolve(resp)
 			} else {
 				prom.Reject(fmt.Errorf("Login Rejected, reason: %v", err))
@@ -69,7 +74,7 @@ func pwdHashBytes(password string) (hashBytes [32]byte) {
 	return
 }
 
-func parseLoginResponseFromGuide(buffer core.PacketBody) (info LoginResponseFromGuide, err error) {
+func parseLoginResponseFromGuide(buffer core.PacketBody) (info LoginResponse, err error) {
 	defer func() {
 		if x := recover(); x != nil {
 			err = x.(error)
@@ -81,7 +86,7 @@ func parseLoginResponseFromGuide(buffer core.PacketBody) (info LoginResponseFrom
 		RoleCreated uint32
 	}
 	core.MustBinaryRead(buffer, &t)
-	info = LoginResponseFromGuide{
+	info = LoginResponse{
 		SessionID:   t.SessionID,
 		RoleCreated: t.RoleCreated != 0,
 	}
